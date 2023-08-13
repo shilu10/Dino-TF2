e61da8b305585522ffe2968d51c3501d14e3968c
Shilu <shilu842003@gmail.com>
2023-08-13 16:48:34 +0530

    added multicrop_wrapper for processing multi_crop images

---
+++ multicrop_wrapper.py
@@ -0,0 +1,41 @@
+import tensorflow as tf 
+from tensorflow import keras 
+import numpy as np 
+
+class MultiCropWrapper(tf.keras.models.Model):
+    def __init__(self, backbone, head, weights=None):
+        super(MultiCropWrapper, self).__init__()
+        backbone.mlp, backbone.head = tf.identity, tf.identity
+        self.head = head
+        self.backbone = backbone
+
+    def unique_consecutive(self, x):
+        shapes = tf.constant([img.shape[1] for img in x], dtype=tf.int32)
+        _, _, output = tf.unique_with_counts(
+                    shapes,
+                    out_idx=tf.dtypes.int32,
+                    name=None
+                )
+
+        crop_inds = np.cumsum(output.numpy(), 0)
+
+        return crop_inds
+
+    def call(self, x):
+        if not isinstance(x, list):
+            x = [x]
+
+        idx_crops = self.unique_consecutive(x)
+        start_indx, output = 0, None
+        for indx, end_idx in enumerate(idx_crops):
+            comb_input = (tf.concat(x[start_indx : end_idx], axis=0))
+            _out = self.backbone(comb_input)
+
+            if isinstance(_out, tuple):
+                _out = _out[0]
+
+            output = _out if output is None else tf.concat([output, _out], axis=0)
+
+            start_indx = end_idx
+
+        return self.head(output)
