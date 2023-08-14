a016b31d30b987e026aef63087480dc7b2266655
Shilu <shilu842003@gmail.com>
2023-08-14 07:28:02 +0530

    added shape_list util function for interpolate_pos_encoding functionality

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from tensorflow import keras 
 import tensorflow as tf 
 import numpy as np
+from typing import *
 
 
 def get_initializer(initializer_range: float = 0.02) -> tf.keras.initializers.TruncatedNormal:
@@ -14,4 +15,28 @@ def get_initializer(initializer_range: float = 0.02) -> tf.keras.initializers.Tr
         `tf.keras.initializers.TruncatedNormal`: The truncated normal initializer.
     """
     return tf.keras.initializers.TruncatedNormal(stddev=initializer_range)
+
+
+def shape_list(tensor: Union[tf.Tensor, np.ndarray]) -> List[int]:
+    """
+    Deal with dynamic shape in tensorflow cleanly.
+
+    Args:
+        tensor (`tf.Tensor` or `np.ndarray`): The tensor we want the shape of.
+
+    Returns:
+        `List[int]`: The shape of the tensor as a list.
+    """
+    if isinstance(tensor, np.ndarray):
+        return list(tensor.shape)
+
+    dynamic = tf.shape(tensor)
+
+    if tensor.shape == tf.TensorShape(None):
+        return dynamic
+
+    static = tensor.shape.as_list()
+
+    return [dynamic[i] if s is None else s for i, s in enumerate(static)]
+
                                                                             
