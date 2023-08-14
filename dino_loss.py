a9c22fafd20ba2f540c876f636dfcf88c9ee6af8
Shilu <shilu842003@gmail.com>
2023-08-14 18:20:45 +0530

    updated the dino_loss with self.center code

--- dino_loss.py
+++ dino_loss.py
@@ -4,21 +4,21 @@ import numpy as np
 
 
 class DinoLoss(tf.keras.losses.Loss):
-  def __init__(self, 
-               out_dim, 
-               ncrops, 
-               warmup_teacher_temp, 
+  def __init__(self,
+               out_dim,
+               ncrops,
+               warmup_teacher_temp,
                teacher_temp,
-               warmup_teacher_temp_epochs, 
-               nepochs, 
+               warmup_teacher_temp_epochs,
+               nepochs,
                student_temp=0.1,
                center_momentum=0.9
             ):
-    
+
     super(DinoLoss, self).__init__()
     self.student_temp = student_temp
     self.center_momentum = center_momentum
-    self.ncrops = ncrops 
+    self.ncrops = ncrops
 
     # we apply a warm up for the teacher temperature because
     # a too high temperature makes the training instable at the beginning
@@ -27,13 +27,13 @@ class DinoLoss(tf.keras.losses.Loss):
                         teacher_temp, warmup_teacher_temp_epochs),
           np.ones(nepochs - warmup_teacher_temp_epochs) * teacher_temp
       ))
-    
+    self.center = tf.zeros_like(out_dim, dtype=tf.float32)
+
   def __call__(self, student_output, teacher_output, epoch):
     """
       Cross-entropy between softmax outputs of the teacher and student networks.
     """
     #super.__call__()
-    self.center = tf.zeros_like(teacher_output, dtype=tf.float32)
     student_out = student_output / self.student_temp
     student_out = tf.split(student_out, self.ncrops)
 
@@ -53,10 +53,12 @@ class DinoLoss(tf.keras.losses.Loss):
         loss = tf.reduce_sum(
                     -q * tf.nn.log_softmax(student_out[v], axis=-1), axis=-1
                 )
+        
         total_loss += tf.math.reduce_mean(loss)
         n_loss_terms += 1
-    
+
     total_loss /= n_loss_terms
+    print(self.center)
     self.update_center(teacher_output)
     return total_loss
 
@@ -65,7 +67,7 @@ class DinoLoss(tf.keras.losses.Loss):
       Update center used for teacher output.
     """
     batch_center = tf.math.reduce_sum(teacher_output, axis=0)
-    batch_center = batch_center / tf.cast(len(teacher_output), tf.float32)
+    batch_center = batch_center / (len(teacher_output))
     self.center = tf.stop_gradient(
             self.center * self.center_momentum
             + batch_center * (1 - self.center_momentum)
